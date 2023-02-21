a0c70367f4613933c50b04a6a176ff47e3eb95aa
didarul381 <didarula128@gmail.com>
2023-02-20 23:55:50 -0800

    add toolkit

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { useLocation } from "react-router-dom";
+import { addToCart, removeFromCart } from "../features/cart/cartSlice";
 
 const ProductCard = ({ product }) => {
+  const {pathname}=useLocation();
+  const dispatch=useDispatch()
   return (
     <div
       className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
-      key={product._id}
+      key={product.id}
     >
       <div className='h-52 w-52 mx-auto'>
         <img src={product.image} alt={product.model} />
@@ -20,9 +25,27 @@ const ProductCard = ({ product }) => {
         </ul>
       </div>
       <div className='flex gap-2 mt-5'>
-        <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'>
+       {
+        !pathname.includes("cart")&&(
+          <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+        onClick={()=>dispatch(addToCart(product))}
+        
+        >
           Add to cart
         </button>
+        
+        
+       )}
+        {
+          pathname.includes("cart")&&(
+            <button className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
+        onClick={()=>dispatch(removeFromCart(product))}
+        
+        >
+          Remove 
+        </button>
+          )
+        }
         <button
           title='Add to wishlist'
           className='bg-indigo-500  py-1 px-2 rounded-full'
